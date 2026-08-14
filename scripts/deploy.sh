b4bc839c19df9a2a95d9f936b20838d3729609eb
Joakim-Aksel-Holm <jholm24@student.sdu.dk>
2026-08-14 10:14:54 +0000

    redis pod, volumeclaim and service setup Co-authered by: Niklas-Hakmann nikha22@student.sdu.dk

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,10 @@ set -e
 
 echo "$KUBECONFIG" | base64 -d > kubeconfig
 
+
+kubectl --kubeconfig kubeconfig apply -f redis/redis-pvc.yml
+kubectl --kubeconfig kubeconfig apply -f redis/redis-pod.yml
+kubectl --kubeconfig kubeconfig apply -f redis/redis-service.yml
+
 kubectl --kubeconfig kubeconfig apply -f backend/backend-pod.yml
 kubectl --kubeconfig kubeconfig apply -f frontend/frontend-pod.yml
